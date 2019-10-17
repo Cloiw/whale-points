ea167b37f15dc62d05dca9edaf8488a8db886465
Cloi <sircli@hotmail.com>
2019-10-16 21:25:08 -0300

    probando iconos para tabs

--- assets/components/Home/index.js
+++ assets/components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Text, Header, Body, Left, Right, Button, Icon, Title, Content, Tab, Tabs } from 'native-base';
+import { Container, Text, Header, Body, TabHeading, Right, Button, Icon, Title, Content, Tab, Tabs } from 'native-base';
 import ScoreTable from '../ScoreTable';
 import Login from '../Login'
 
@@ -8,10 +8,18 @@ const Home = () => (
   <Container>
         <Header hasTabs />
         <Tabs>
-          <Tab heading="Puntajes">
+          <Tab heading={ 
+            <TabHeading>
+              <Icon type="MaterialCommunityIcons" name="table-of-contents" />
+              <Text>Puntajes</Text>
+            </TabHeading>}>
             <ScoreTable />
           </Tab>
-          <Tab heading="+ - Puntos">
+          <Tab  heading={ 
+            <TabHeading>
+              <Icon type="MaterialCommunityIcons" name="calculator-variant" />
+              <Text>Puntos</Text>
+            </TabHeading>}>
             <Login />
           </Tab>
         </Tabs>
