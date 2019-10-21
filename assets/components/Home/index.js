10fa1131e0e88707b39b750ed4303d704c4d441d
Cloi <sircli@hotmail.com>
2019-10-21 19:04:59 -0300

    vista home

--- assets/components/Home/index.js
+++ assets/components/Home/index.js
@@ -1,43 +1,55 @@
 import React from 'react';
-import { Container, Text, Header, Body, TabHeading, Right, Button, Icon, Title, Content, Tab, Tabs } from 'native-base';
+import { Image } from 'react-native';
+import { Container, Text, Header, Grid, TabHeading, Row, Button, Icon, Col, Content, Tab, Tabs, Thumbnail } from 'native-base';
 import ScoreTable from '../ScoreTable';
 import Login from '../Login'
 
 const Home = () => (
 
-  <Container>
-        <Header hasTabs />
-        <Tabs>
+  <Container style={{backgroundColor:'#E7E7E7'}}>
+    <Grid>
+    <Row size={20} style={{justifyContent:'center', alignItems: 'center'}}>
+      <Col>
+        <Thumbnail large source={require('../../icon.png')} style={{alignSelf: 'center'}} />
+      </Col>
+      <Col>
+        <Text style={{fontSize: 30, alignSelf: 'center', fontFamily: 'Baloo'}} >Nombre</Text>
+        <Text style={{fontSize: 30, alignSelf: 'flex-end', fontFamily: 'Baloo'}} >500</Text>
+      </Col>
+    </Row>
+    <Row size={80} style={{}}>
+        <Tabs style={{backgroundColor: '#008AC5'}}>
           <Tab heading={ 
             <TabHeading>
               <Icon type="MaterialCommunityIcons" name="table-of-contents" />
-              <Text>Puntaje</Text>
+              <Text></Text>
             </TabHeading>}>
             <ScoreTable />
           </Tab>
-          
-          <Tab  heading={ 
+          <Tab heading={ 
             <TabHeading>
               <Icon type="MaterialCommunityIcons" name="calculator-variant" />
-              <Text>Puntos</Text>
+              <Text></Text>
             </TabHeading>}>
             <Login />
           </Tab>
           <Tab heading={ 
             <TabHeading>
               <Icon type="AntDesign" name="exclamationcircle" />
-              <Text>Solicitudes</Text>
+              <Text></Text>
             </TabHeading>}>
             <ScoreTable />
           </Tab>
           <Tab heading={ 
             <TabHeading>
               <Icon type="MaterialIcons" name="history" />
-              <Text>Historial </Text>
+              <Text></Text>
             </TabHeading>}>
             <ScoreTable />
           </Tab>
         </Tabs>
+        </Row>
+        </Grid>
       </Container>
 )
 
