517f2a7c67be24e58fd758386792f61a9d12f707
Cloi <sircli@hotmail.com>
2019-10-22 11:26:57 -0300

    agregando numero en lista

--- assets/components/InfoCard/index.js
+++ assets/components/InfoCard/index.js
@@ -7,6 +7,7 @@ class InfoCard extends Component {
     return (
       <Card>
         <CardItem>
+            <Text  style={{fontSize: 30, fontFamily: 'Baloo', marginRight: 10}} >{this.props.ranking}</Text>
           <Left>
             <Thumbnail large source={{uri:this.props.image}} />
             <Body>
--- assets/components/ScoreTable/index.js
+++ assets/components/ScoreTable/index.js
@@ -6,8 +6,8 @@ const ScoreTable = () => (
 
   <Container style={{backgroundColor:'#E7E7E7'}}>
     <Content>
-      <InfoCard image={'https://i.ibb.co/z5h63TR/lokibb.jpg'} name={"cloiw"} points={57777}/>
-      <InfoCard image={'https://i.ibb.co/z5h63TR/lokibb.jpg'} name={"cloiwclon"} points={-577}/>
+      <InfoCard ranking={1} image={'https://i.ibb.co/z5h63TR/lokibb.jpg'} name={"cloiw"} points={57777}/>
+      <InfoCard ranking={2} image={'https://i.ibb.co/z5h63TR/lokibb.jpg'} name={"cloiwclon"} points={-577}/>
     </Content>
   </Container>
 )
