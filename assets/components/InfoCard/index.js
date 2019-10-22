f01b04ba5efe6975eebf74661a4962ed6472f9b4
Cloi <sircli@hotmail.com>
2019-10-22 10:30:50 -0300

    infoCard OK

--- assets/components/InfoCard/index.js
+++ assets/components/InfoCard/index.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native'
-import { Container, Right, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { Card, CardItem, Thumbnail, Text, Left, Body } from 'native-base';
 
-export default class InfoCard extends Component {
+class InfoCard extends Component {
   render() {
     return (
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail large source={{uri:this.props.image}} />
-                <Body>
-                  <Text style={{fontSize: 30, fontFamily: 'Baloo'}}>{this.props.name}</Text>
-                  <Text style={this.props.points < 0 ? styles.redText : styles.blueText}>{this.props.points} 🐳</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+      <Card>
+        <CardItem>
+          <Left>
+            <Thumbnail large source={{uri:this.props.image}} />
+            <Body>
+              <Text style={{fontSize: 30, fontFamily: 'Baloo'}}>{this.props.name}</Text>
+              <Text style={this.props.points < 0 ? styles.redText : styles.blueText}>{this.props.points} 🐳</Text>
+            </Body>
+          </Left>
+        </CardItem>
+      </Card>
     );
   }
 }
@@ -30,6 +30,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Baloo',
     color: '#F10000'
-  },
- 
+  }
 })
+
+export default InfoCard;
