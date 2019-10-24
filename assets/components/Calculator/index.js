4d4a8819f61d3b806bd30396fc0cd879d1a74116
Cloi <sircli@hotmail.com>
2019-10-24 19:45:23 -0300

    agregando botones sumar, restar

--- assets/components/Calculator/index.js
+++ assets/components/Calculator/index.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native'
-import { Col, Row, Thumbnail, Text, Content, Grid } from 'native-base';
+import { StyleSheet, TextInput } from 'react-native'
+import { Col, Row, Thumbnail, Text, Button, Grid } from 'native-base';
 
 class Calculator extends Component {
   render() {
     return (
       <Grid style={{backgroundColor: 'white'}}>
-        <Row size={20}>
+        <Row size={30}>
           <Col style={{alignItems: 'center', justifyContent: 'center'}}>
             <Thumbnail large source={{uri:this.props.image}} />
           </Col>
           <Col>
-            <Row size={1} style={{ width:'90%', alignSelf: 'center'}}>
+            <Row size={2} style={{ width:'90%', alignSelf: 'center'}}>
               <Text style={styles.nameText}>{this.props.name}</Text>
             </Row>
-            <Row size={2} style={styles.pointsRow}>
-              <Text style={{fontSize: 40, fontFamily: 'Baloo', marginRight: 10}}>00005</Text>
+            <Row size={3} style={styles.pointsRow}>
+              <TextInput editable  style={styles.textInput} maxLength={7} />
+            </Row>
+            <Row size={1} style={{margin: 10, justifyContent:'space-between'}}>
+              <Button style={styles.addOrSubButton}>
+                <Text style={styles.buttonText}>SUMAR</Text>
+              </Button>
+              <Button style={styles.addOrSubButton}>
+                <Text style={styles.buttonText}>RESTAR</Text>
+              </Button>
             </Row>
           </Col>
         </Row>
@@ -41,6 +49,23 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     justifyContent: 'flex-end', 
     borderRadius: 10
+  },
+  textInput: {
+    width:'100%',
+    textAlign:'right',
+    fontSize: 35,
+    fontFamily: 'Baloo',
+    marginRight: 10
+  },
+  addOrSubButton: {
+    backgroundColor: '#008AC5',
+    width: '45%', 
+    height: '90%',
+    justifyContent:'center'
+  },
+  buttonText: {
+    fontFamily: 'Baloo',
+    fontSize: 15,
   }
 })
 
