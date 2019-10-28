934d95502b8ab6f1d85d5370200903ca8ad08747
Cloi <sircli@hotmail.com>
2019-10-28 09:42:49 -0300

    creando login

--- assets/components/Login/index.js
+++ assets/components/Login/index.js
@@ -4,14 +4,19 @@ import { Container, Header, Content, Form, Item, Input, Label, Button, Text, Gri
 
 
 class Login extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { username: "", password: ""}
+  }
   render() {
+    console.log(this.state.username, this.state.password)
     return (
       <Container style={{backgroundColor:'#E7E7E7'}}>
         <Grid>
           <Row size={40} style={{justifyContent:'center', alignItems: 'center'}}>
             <Col>
-            <Text style={{fontSize: 80, alignSelf: 'center'}} >🐳</Text>
-            <Text style={{fontSize: 30, alignSelf: 'center', fontFamily: 'Baloo'}} >WhalePoints</Text>
+              <Text style={{fontSize: 80, alignSelf: 'center'}} >🐳</Text>
+              <Text style={{fontSize: 30, alignSelf: 'center', fontFamily: 'Baloo'}} >WhalePoints</Text>
             </Col>
           </Row>
           <Row size={25}  style={{alignItems:'center'}}>
@@ -19,18 +24,18 @@ class Login extends Component {
               <Form style={{width:'90%', justifyContent:'center', alignSelf: 'center'}}>
                 <Item style={styles.item} stackedLabel>
                   <Label style={styles.label}>Usuario</Label>
-                  <Input style={styles.input} />
+                  <Input onChangeText={val => this.setState({ username: val })} style={styles.input} />
                 </Item>
                 <Item style={styles.item} stackedLabel last>
                   <Label style={styles.label}>Contraseña</Label>
-                  <Input style={styles.input} />
+                  <Input secureTextEntry={true} onChangeText={val => this.setState({ password: val })} style={styles.input} />
                 </Item>
               </Form>
             </Col>
           </Row>
           <Row size={35} style={{justifyContent:'center', alignItems: 'center'}}>
             <Button onPress={() => this.props.changeLoginStatus()}style={{backgroundColor: '#008AC5', width: '90%', height: '20%', alignSelf: 'flex-start'}}>
-              <Text>Entrar</Text>
+              <Text uppercase={false}>Entrar</Text>
             </Button>
           </Row>
         </Grid>
