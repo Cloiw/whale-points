22bdc7323a3a78b834649978d51885fbbe27f1bd
Cloi <sircli@hotmail.com>
2019-10-28 13:57:54 -0300

    signup

---
+++ assets/components/Signup/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content } from 'native-base';
+
+const Signup = () => (
+
+  <Container style={{backgroundColor:'#E7E7E7'}}>
+    <Content>
+    </Content>
+  </Container>
+)
+
+export default Signup;
